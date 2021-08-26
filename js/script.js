ec74fda43157acd45df4d7265328ac5d61ebe65d
leonardobarone <leonardobarone26@gmail.com>
2021-08-26 15:27:07 +0200

    js fatto

--- js/script.js
+++ js/script.js
@@ -1,8 +1,28 @@
-alert("ciao")
 
-// Consegna:
 // 1. chiedi all’utente il cognome
 // 2. inseriscilo in un array con altri cognomi: ‘Bianchi’, ‘Neri’, ‘Rossi’, ‘Verdi’, ‘Gialli’
+var arrayCognomi = [
+    "Bianchi",
+    "Neri",
+    "Rossi",
+    "Verdi",
+    "Gialli"
+];
+
+var cognome = arrayCognomi.push(prompt("Qual è il tuo cognome?"));
+
+for (var i = 0; i < arrayCognomi.length; i++) {
+    
+    arrayCognomi.sort();
+    console.log(`${arrayCognomi[i]} è il ${i + 1}° cognome` );
+    
+}
+
+// console.log(arrayCognomi.sort());
+
+
+
+
 // 3. stampa la lista ordinata alfabeticamente
 // 4. scrivi anche la posizione "umana" (partendo da 1) della lista in cui il nuovo utente si trova
 // Consigli del giorno:
